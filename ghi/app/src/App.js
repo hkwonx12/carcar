79a2ce4af68ca7f060bb47d5d6b3cb95db0e2458
Wanda <wanda.mccrae@gmail.com>
2023-03-06 18:21:30 -0500

    Model list and create model form

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import ModelsList from './inventorycomponents/ModelsList';
+import ModelForm from './inventorycomponents/ModelForm';
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="models">
+            <Route index element={<ModelsList />} />
+            <Route path="new" element={<ModelForm />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
