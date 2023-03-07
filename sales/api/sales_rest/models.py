00391f161af7ed1b43aecaf5d117cae1e99d0119
Wanda <wanda.mccrae@gmail.com>
2023-03-07 13:20:38 -0500

    Working Poller

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Keep track of our current vehicle inventory.
+class InventoryVO(models.Model):
+    # Because the id of the VO may not match the id of the model instance,
+    # use the URL returned by the model instance as its identifier.
+    import_href = models.CharField(max_length=200, unique=True)
+    vin = models.CharField(max_length=17, unique=True)
+
+    def __str__(self):
+        return self.vin
